7ae4d3f541ecd5b22f85be177172a5879c8472f3
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-01 14:52:01 +0200

    Create intent handling

--- GenAIFinder/app/src/main/java/io/krakau/genaifinder/MainActivity.kt
+++ GenAIFinder/app/src/main/java/io/krakau/genaifinder/MainActivity.kt
@@ -1,6 +1,8 @@
 package io.krakau.genaifinder
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,6 +11,9 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.webkit.WebView
+import android.widget.Button
+import android.widget.TextView
 import io.krakau.genaifinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,17 +21,52 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private var textIntent: TextView? = null
+    private var textImages: TextView? = null
+    private var webview: WebView? = null
+    private var searchBtn: Button? = null
+
+    private var url: String = "https://x.com/spsth/status/1851232714399039837?t=2iKpQnarnkM3IDK8NXTVfg&s=19"
+    //private var url: String = "https://pbs.twimg.com/media/GbDmULJXYAAsPBQ?format=jpg&name=small";
+    //private var url: String = "https://www.spiegel.de/";
+    //private var url: String = "https://www.instagram.com/andy.grote/p/DH3tiQ5MUs6/?img_index=1";
+    //private var url: String = "";
+
+    private var imageUrls: Array<String?> = emptyArray()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // bindings
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
+        textIntent = binding.textIntent
+        textImages = binding.textImages
+        webview = binding.webview
+        searchBtn = binding.searchBtn
+
+        // check intent
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/plain" == intent.type) {
+                    handleSendText(intent) // Handle text being sent
+                }
+            }
+        }
 
         /*val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)*/
+    }
+
+    private fun handleSendText(intent: Intent) {
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            Log.d("Intent", it)
+            url = it
+            // Update UI to reflect text being shared
+            textIntent?.setText(it)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
