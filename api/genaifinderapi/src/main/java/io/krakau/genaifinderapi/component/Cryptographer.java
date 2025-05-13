050bf8ec889b4e03223696b3a2f65042ccfd2ad3
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-13 20:45:15 +0200

    Create Cryptographer component

---
+++ api/genaifinderapi/src/main/java/io/krakau/genaifinderapi/component/Cryptographer.java
@@ -0,0 +1,24 @@
+package io.krakau.genaifinderapi.component;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Dominik
+ */
+@Component
+public class Cryptographer {
+    
+    public Cryptographer() {
+    
+    }
+    
+    public void encrypt(String privateKey, String message) {
+        
+    }
+    
+    public void decrypt(String publicKey, String message) {
+        
+    }
+    
+}
