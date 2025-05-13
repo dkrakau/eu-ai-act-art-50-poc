8e76711a2e701a019d2fc94c9c013ba2c10595ce
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-13 20:44:11 +0200

    Create AssetService

--- api/genaifinderapi/src/main/java/io/krakau/genaifinderapi/repository/AssetRepository.java
+++ api/genaifinderapi/src/main/java/io/krakau/genaifinderapi/repository/AssetRepository.java
@@ -3,6 +3,7 @@ package io.krakau.genaifinderapi.repository;
 import io.krakau.genaifinderapi.schema.mongodb.Asset;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
@@ -11,6 +12,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  */
 public interface AssetRepository extends MongoRepository<Asset, String>{
     
-     Slice<Asset> findAllAssets(Pageable page);
+    @Aggregation(pipeline = {
+        "{$match:{}}"
+    })
+    Slice<Asset> findAllAssets(Pageable page);
     
 }
---
+++ api/genaifinderapi/src/main/java/io/krakau/genaifinderapi/service/AssetService.java
@@ -0,0 +1,28 @@
+package io.krakau.genaifinderapi.service;
+
+import io.krakau.genaifinderapi.repository.AssetRepository;
+import io.krakau.genaifinderapi.schema.mongodb.Asset;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Dominik
+ */
+@Service
+public class AssetService {
+    
+    private AssetRepository assetRepository;
+    
+    @Autowired
+    public AssetService(AssetRepository assetRepository) {
+        this.assetRepository = assetRepository;
+    }
+    
+    public Slice<Asset> findAllAssets(Pageable pageable) {
+        return this.assetRepository.findAllAssets(pageable);
+    }
+    
+}
