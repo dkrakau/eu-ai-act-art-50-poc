73a28c54d8d1a34989c28ee05e549075e9ee8dbb
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-13 13:37:04 +0200

    initial commit for api project

---
+++ api/genaifinderapi/src/main/java/io/krakau/genaifinderapi/GenaifinderapiApplication.java
@@ -0,0 +1,13 @@
+package io.krakau.genaifinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaifinderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenaifinderapiApplication.class, args);
+	}
+
+}
