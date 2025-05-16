e6729fd2ffc0c2b498af7da9b671f9f6b6239fe6
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-16 10:57:27 +0200

    Add example url to findImage endpoint

--- api/genaifinderapi/src/main/java/io/krakau/genaifinderapi/controller/FindController.java
+++ api/genaifinderapi/src/main/java/io/krakau/genaifinderapi/controller/FindController.java
@@ -3,6 +3,7 @@ package io.krakau.genaifinderapi.controller;
 import io.krakau.genaifinderapi.schema.mongodb.Asset;
 import io.krakau.genaifinderapi.service.FindService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -13,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,7 +51,10 @@ public class FindController {
                     @Content(schema = @Schema(implementation = Error.class), mediaType = "application/json")})
     })
     @GetMapping("/image")
-    public ResponseEntity<List<Asset>> findImage(@RequestParam("url") String url) throws Exception {
+    public ResponseEntity<List<Asset>> findImage(
+            @PathVariable("url")
+            @Parameter(description="url", example="https://pbs.twimg.com/media/GrAdSm-WAAA0yxV?format=jpg&name=large")
+            @RequestParam  String url) throws Exception {
         return ResponseEntity.ok().body(this.findService.findImage(url));
     }
     
