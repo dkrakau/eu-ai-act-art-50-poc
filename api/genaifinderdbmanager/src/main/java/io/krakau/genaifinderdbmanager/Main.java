76b0896062f6bfe2d414765fd992b49dd1b464d0
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-16 18:33:28 +0200

    initial commit

---
+++ api/genaifinderdbmanager/src/main/java/io/krakau/genaifinderdbmanager/Main.java
@@ -0,0 +1,13 @@
+package io.krakau.genaifinderdbmanager;
+
+/**
+ *
+ * @author Dominik
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
