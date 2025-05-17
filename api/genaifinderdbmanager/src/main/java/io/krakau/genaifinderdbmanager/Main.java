fd3220897c8462a8373ec2ec6c51957d0d727344
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-17 14:39:44 +0200

    Setup Production and Develpment section in Main class

--- api/genaifinderdbmanager/src/main/java/io/krakau/genaifinderdbmanager/Main.java
+++ api/genaifinderdbmanager/src/main/java/io/krakau/genaifinderdbmanager/Main.java
@@ -10,21 +10,23 @@ import java.io.IOException;
  */
 public class Main {
 
+    private static final String APPLICATION_PROPOERTIES = "application.properties";
     private static DatabaseManager databaseManager;
     private static ImportService importService;
 
     public static void main(String[] args) throws IOException {
-
-        initArgsReader(args);
-
-        // #####################################################################
-        EnvironmentVariables env = new EnvironmentVariables("applicatoin.properties");
+        // ###########################  PRODUCTION  ############################
+        EnvironmentVariables env = new EnvironmentVariables(APPLICATION_PROPOERTIES);
         databaseManager = new DatabaseManager(env);
-        importService = new ImportService(env, "ai-images/data-images.json");
-
+        initArgsReader(args);
+        // ##########################  DEVELOPMENT  ############################
+//        EnvironmentVariables env = new EnvironmentVariables(APPLICATION_PROPOERTIES);
+//        databaseManager = new DatabaseManager(env);
 //        databaseManager.create();
 //        databaseManager.drop();
-        importService.importData();
+//        importService = new ImportService(env, "ai-images/data-images.json");
+//        importService.importData();
+        // #####################################################################
     }
 
     public static void initArgsReader(String[] args) throws IOException {
@@ -36,14 +38,14 @@ public class Main {
             } else if (args[0].equals("-i") || args[0].equals("--import")) {
                 if (args.length > 1) {
                     String filePath = args[1];
-                    importService = new ImportService(new EnvironmentVariables("applicatoin.properties"), filePath);
+                    importService = new ImportService(new EnvironmentVariables(APPLICATION_PROPOERTIES), filePath);
                     importService.importData();
                 } else {
                     System.out.println("Path to file is missing. See help.");
                     databaseManager.help();
                 }
             } else if (args[0].equals("-s") || args[0].equals("--stats")) {
-               databaseManager.stats();
+                databaseManager.stats();
             } else if (args[0].equals("-h") || args[0].equals("--help")) {
                 databaseManager.help();
             } else {
