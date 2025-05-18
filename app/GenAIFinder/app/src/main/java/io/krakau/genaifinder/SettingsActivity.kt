801bd9ad87661369399eed8097d5a31ad66b6ad2
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-18 16:27:20 +0200

    First implementation of SettingsActivity

---
+++ app/GenAIFinder/app/src/main/java/io/krakau/genaifinder/SettingsActivity.kt
@@ -0,0 +1,85 @@
+package io.krakau.genaifinder
+
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import java.io.Serializable
+
+class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var settingsBack: ImageView
+
+    private lateinit var callingActivity: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        settingsBack = findViewById(R.id.settingsBack)
+
+        callingActivity = intent.getStringExtra("callingActivity")!!
+
+        settingsBack.setOnClickListener {
+            startActivity(goBackIntent(callingActivity))
+        }
+    }
+
+    fun goBackIntent(sourceActivity: String): Intent {
+        var intent = Intent(this@SettingsActivity, FinderActivity::class.java)
+        if(sourceActivity.equals("io.krakau.genaifinder.MainActivity")) {
+            intent = Intent(this@SettingsActivity, MainActivity::class.java)
+        }
+        if(sourceActivity.equals("io.krakau.genaifinder.ImageGalleryActivity")) {
+            intent = Intent(this@SettingsActivity, ImageGalleryActivity::class.java)
+        }
+        if(sourceActivity.equals("io.krakau.genaifinder.FinderActivity")) {
+            intent = Intent(this@SettingsActivity, FinderActivity::class.java)
+        }
+        return intent
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this@SettingsActivity, SettingsActivity::class.java).apply {
+                    putExtra("callingActivity", callingActivity)
+                }.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                true
+            }
+            R.id.action_information -> {
+                startActivity(Intent(this@SettingsActivity, InformationActivity::class.java).apply {
+                    putExtra("callingActivity", callingActivity)
+                }.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
