590814a76649dcf53b429a4e687d6513e0d59300
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-18 19:27:42 +0200

    Implement listener for switch and spinner

--- app/GenAIFinder/app/src/main/java/io/krakau/genaifinder/SettingsActivity.kt
+++ app/GenAIFinder/app/src/main/java/io/krakau/genaifinder/SettingsActivity.kt
@@ -2,10 +2,19 @@ package io.krakau.genaifinder
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.Spinner
+import android.widget.Switch
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.ui.AppBarConfiguration
 
 class SettingsActivity : AppCompatActivity() {
@@ -13,6 +22,8 @@ class SettingsActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     private lateinit var settingsBack: ImageView
+    private lateinit var darkModeSwitch: Switch
+    private lateinit var serverUrlSpinner: Spinner
 
     private lateinit var callingActivity: String
 
@@ -20,13 +31,39 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         setSupportActionBar(findViewById(R.id.toolbar))
+        val applicationContext = this;
 
         settingsBack = findViewById(R.id.settingsBack)
+        darkModeSwitch = findViewById(R.id.darkModeSwitch)
+        serverUrlSpinner = findViewById(R.id.serverUrlSpinner)
 
+        // Get calling activity
         callingActivity = intent.getStringExtra("callingActivity")!!
-
         settingsBack.setOnClickListener {
             startActivity(goBackIntent(callingActivity))
+        }
+
+        // Setup dark mode switch
+        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                Toast.makeText(applicationContext, "DarkMode: $isChecked", Toast.LENGTH_SHORT).show()
+            } else {
+                // AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Toast.makeText(applicationContext, "DarkMode: $isChecked", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Setup server url spinner
+        val adapter = ArrayAdapter.createFromResource(applicationContext, R.array.serverUrls, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        serverUrlSpinner.adapter = adapter
+        serverUrlSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                Toast.makeText(applicationContext, "Selected: $selectedItem", Toast.LENGTH_SHORT).show()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {/* Another interface callback */}
         }
     }
 
