39ced7c573e82b2bea40ac46193e98437ba9021c
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-19 12:47:49 +0200

    Add dependencies for retrofit to handle http requests

--- app/GenAIFinder/app/build.gradle.kts
+++ app/GenAIFinder/app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     implementation("com.caverock:androidsvg-aar:1.4")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
