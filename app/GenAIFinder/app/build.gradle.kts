503e64d985a7e729eb496a1666bf0d745dadbd20
Dominik Krakau <dominik.krakau@outlook.de>
2025-05-27 14:22:33 +0200

    Import dependencies and plugins for glide svg support

--- app/GenAIFinder/app/build.gradle.kts
+++ app/GenAIFinder/app/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.internal.Kapt3GradleSubplugin.Companion.findKaptConfiguration
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -53,10 +56,12 @@ dependencies {
 
     // custom imports
     implementation(libs.jsoup)
-    implementation(libs.picasso)
-    implementation("com.caverock:androidsvg-aar:1.4")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
+
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.caverock:androidsvg:1.4")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
